1399d796ab8ade45b6a09f282b8ee8e6b4a632b0
Robin AUBERTIN <robin.aubertin.simplon@gmail.com>
2021-10-27 14:35:50 +0200

    update strategy

--- Strategy.php
+++ Strategy.php
@@ -1,2 +1,46 @@
 <?php
 
+class Context
+{
+    private $strategy;
+
+    public function __construct(Strategy $strategy)
+    {
+        $this->strategy = $strategy;
+    }
+
+    public function setStrategy(Strategy $strategy)
+    {
+        $this->strategy = $strategy;
+    }
+
+    public function doSomething(): void
+    {
+        $filename = "text.md";
+        $content = "Test content";
+    }
+}
+
+interface Strategy
+{
+    public function fileAction($filename, $content);
+}
+
+class Printer implements Strategy
+{
+    public function fileAction($filename, $content)
+    {
+        echo $filename;
+        echo "<br>";
+        echo $content;
+        return;
+    }
+}
+
+class CreateFile implements Strategy
+{
+    public function fileAction($filename, $content)
+    {
+        return file_put_contents($filename, $content);
+    }
+}
